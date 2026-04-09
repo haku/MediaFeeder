df47158d7fbb6e4cff193836ad5d1759a5d87067
Foxocube <git@foxocube.xyz>
2026-04-09 23:53:02 +0100

    Add flake config for scripts

---
+++ scripts/default.nix
@@ -0,0 +1,203 @@
+_: {
+  perSystem = {
+    pkgs,
+    lib,
+    ...
+  }: let
+    pbpy = pkgs.python3.withPackages (p:
+      with p; [
+        grpcio-tools
+        mypy-protobuf
+      ]);
+
+    mfprotos = pkgs.python3Packages.buildPythonPackage {
+      name = "mfprotos";
+      src = ../MediaFeeder/Services;
+      dontUnpack = true;
+      format = "other";
+      propagatedBuildInputs = [pkgs.protobuf];
+      buildPhase = ''
+        runHook preBuild
+        ${pbpy}/bin/python3 -m grpc_tools.protoc \
+          --plugin=${pbpy.pkgs.mypy-protobuf}/bin/protoc-gen-mypy \
+          --proto_path=$src \
+          --python_out=. \
+          --grpc_python_out=. \
+          --mypy_out=. \
+          $src/Api.proto
+        runHook postBuild
+      '';
+      installPhase = ''
+        runHook preInstall
+        mkdir -p $out/${pbpy.sitePackages}
+        install -m644 -D *.py *.pyi $out/${pbpy.sitePackages}/
+        runHook postInstall
+      '';
+    };
+
+    python = pkgs.python3.withPackages (p:
+      with p; [
+        mfprotos
+        aiofiles
+        beautifulsoup4
+        fasteners
+        grpcio
+        grpcio-tools
+        ipython
+        lxml
+        pyatv
+        pychromecast
+        pyyaml
+        pyytlounge
+        requests
+        types-protobuf
+        types-pyyaml
+        types-requests
+      ]);
+
+    bridges = pkgs.stdenv.mkDerivation {
+      name = "mediafeeder-bridges";
+      src = ../scripts;
+      propagatedBuildInputs = [python];
+
+      installPhase = ''
+        runHook preInstall
+
+        install -Dt $out chromecast_bridge.py common.py auth.py config_file.py
+
+        runHook postInstall
+      '';
+
+      meta = {
+        description = "MediaFeeder Bridges";
+        homepage = "https://github.com/girlpunk/mediafeeder";
+      };
+    };
+
+    bridgesWrapper = pkgs.writeShellScriptBin "chromecast-bridge" ''
+      ${python}/bin/python3 ${bridges}/chromecast_bridge.py
+    '';
+
+    allBridges = pkgs.symlinkJoin {
+      name = "mediafeeder-bridges";
+      paths = [bridges bridgesWrapper];
+    };
+  in {
+    make-shells.scripts = {
+      name = "MediaFeeder Scripts";
+      #commands = [ {
+      #    name = "grpc-rebuild";
+      #    help = "Re-generate python GRPC bindings";
+      #    command = "${pkgs.python3}/bin/python -m grpc_tools.protoc --proto_path=../MediaFeeder/Services --python_out=. --grpc_python_out=. ../MediaFeeder/Services/Api.proto";
+      #  }
+      #];
+      packages = with pkgs;
+        [
+          git
+          grpc_cli
+        ]
+        ++ [python];
+    };
+
+    packages = {
+      mediafeeder-bridges = allBridges;
+    };
+
+    apps.mediafeeder-bridges = {
+      program = allBridges;
+    };
+
+    treefmt = {
+      # Enable the Python formatters
+      programs.ruff-format = {
+        enable = true;
+        lineLength = 180;
+      };
+      programs.ruff-check = {
+        enable = true;
+        extendSelect = [
+          "A"
+          "ANN"
+          "ARG"
+          "ASYNC"
+          "B"
+          "BLE"
+          "C"
+          "C4"
+          "C90"
+          "COM"
+          "D"
+          "DOC"
+          "DTZ"
+          "E"
+          "EM"
+          "EXE"
+          "F"
+          "F"
+          "FA"
+          "FBT"
+          "FIX"
+          "FLY"
+          "FURB"
+          "G"
+          "I"
+          "ICN"
+          "INP"
+          "INT"
+          "ISC"
+          "LOG"
+          "N"
+          "PERF"
+          "PGH"
+          "PIE"
+          "PL"
+          "PTH"
+          "PYI"
+          "Q"
+          "Q"
+          "RET"
+          "RSE"
+          "RUF"
+          "S"
+          "SIM"
+          "SLF"
+          "T10"
+          "T20"
+          "TC"
+          "TD"
+          "TID"
+          "TRY"
+          "UP"
+          "W"
+          "W"
+          "YTT"
+        ];
+      };
+    };
+
+    pre-commit = {
+      settings = {
+        hooks = {
+          build-grpc = {
+            enable = true;
+            name = "Build GRPC Generate Files";
+            entry = "${pkgs.python3}/bin/python -m grpc_tools.protoc --proto_path=. --python_out=. --grpc_python_out=. Api.proto";
+            # language = "system";
+            pass_filenames = false;
+            stages = ["pre-commit"];
+
+            before = ["mypy"];
+          };
+
+          mypy.enable = true;
+          mypy.extraPackages = python;
+          mypy.args = ["--follow-untyped-imports"];
+
+          ruff.enable = true;
+
+          ruff-format.enable = true;
+        };
+      };
+    };
+  };
+}
